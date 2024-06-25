b7068260d2305c491fb8374b9940833c23622249
Fauzi <Fauzi190198@gmail.com>
2024-06-25 23:19:54 +0700

    fix: allow cors

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import adminRoutes from './routes/admin.routes';
 import cashierRoutes from './routes/cashier.routes';
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/api', adminRoutes);
 app.use('/api', cashierRoutes);
